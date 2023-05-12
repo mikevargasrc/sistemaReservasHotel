ae85804be9b363f46fae0e02dc768cc6e713c9b7
MIKE <maykolvargas98@hotmail.com>
2023-05-12 11:39:48 -0500

    iniciando con base de gestion de clientes edit remove y update

---
+++ src/main/java/com/mycompany/sistemagestionclientes/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sistemagestionclientes;
+
+import com.mycompany.sistemagestionclientes.forms.Formulario;
+
+/**
+ *
+ * @author Mike
+ */
+public class Main {
+    public static void main(String[] args){
+    Formulario ventana = new Formulario();
+    ventana.show();
+    }
+}
